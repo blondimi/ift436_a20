819870ab4c61fc71fccdf241d8ec9ff9c4ae6d6d
Michael Blondin <michael.blondin@tum.de>
2020-11-05 13:58:20 -0500

    Max. circ.: annotations + ex. doublons

--- chapitre5/max_circulaire.py
+++ chapitre5/max_circulaire.py
@@ -1,12 +1,15 @@
-#  Pré-condition: séquence s non vide, sans doublon et ordonnée circulairement
+from random import randint
+
+#  Pré-condition: séquence ordonnée circulairement t.q. |s| = 1
+#                 ou (|s| ≥ 2 et le premier élément diffère du dernier)
 # Post-condition: max(s)
 def max_circ(s):
     n = len(s)
 
-    #  Pré-condition: s[lo] > s[hi]
+    #  Pré-condition: lo < hi, s[lo] > s[hi] et s ord. circulairement
     # Post-condition: max(s[lo : hi])
     def aux(lo, hi):
-        if hi - lo <= 1:
+        if hi - lo == 1:
            return s[lo]
         else:
             mid = (lo + hi) // 2
@@ -16,13 +19,24 @@ def max_circ(s):
             elif s[mid] > s[hi]:
                 return aux(mid, hi)
             else:
-                assert(False) # Impossible car séquence sans doublon
+                assert(False) # Impossible car s[lo] > s[hi]
 
     return aux(0, n - 1) if s[0] > s[n - 1] else s[n - 1]
 
-# Exemple des notes de cours
+# Exemples
 if __name__ == "__main__":
+    # Exemple des notes de cours
     s = [10, 12, 19, 1, 3, 4, 7]
 
     print("     s =", s)
     print("max(s) =", max_circ(s))
+    print()
+
+    # Exemple avec doublons mais pas aux deux bouts #    ##
+    s = [6, 6, 7, 7, 0, 0, 1, 1, 2, 3, 5, 5]        #  ####
+                                                    #  ####      ##
+    print("     s =", s)                            #  ####      ##
+    print("max(s) =", max_circ(s))                  #  ####     ###
+                                                    #  ####    ####
+                                                    #  ####  ######
+                                                    #  ¯¯¯¯¯¯¯¯¯¯¯¯
