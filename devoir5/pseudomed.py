88880a6d33e85122fa0f5c76b12902256c6b2cea
Michael Blondin <michael.blondin@tum.de>
2020-11-26 16:41:52 -0500

    Implémentation de pseudomed

---
+++ devoir5/pseudomed.py
@@ -0,0 +1,24 @@
+from random import choice
+
+def pseudomed(s, k):
+    t = []
+    
+    for _ in range(k):
+        t.append(choice(s))
+
+    t.sort()
+
+    return t[len(t) // 2]
+
+if __name__ == "__main__":
+    repetitions = 10000
+    num_erreurs = 0
+
+    s = [1, 2, 3, 4]
+    k = 3
+    
+    for _ in range(repetitions):
+        x = pseudomed(s, k)
+        
+        if x == 1 or x == 4: # Attention, à changer manuellement si s change
+            num_erreurs += 1
